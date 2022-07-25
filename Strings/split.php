3bbd034a6fe016cbc14688ef28dab9cba4990daa
Hemanth Kumar <90673550+Hemanthoo7@users.noreply.github.com>
2022-07-25 16:48:34 +0530

    String types commit
    String types commit

---
+++ Strings/split.php
@@ -0,0 +1,9 @@
+<?php
+
+//split the following string
+
+$num = 7838399877;
+
+echo chunk_split($num,2,':');
+
+?>
---
+++ Strings/sprintf.php
@@ -0,0 +1,9 @@
+<?php
+
+// Format values in currency style
+$a = 872.9;
+$b = 981.88;
+
+echo sprintf("%1.2f",$a+$b);
+
+?>
